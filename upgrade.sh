39dfe3daf88ff9c0116ac1b1e8ff5110292884d3
rogierg <rogierg@hotmail.com>
2022-09-27 12:52:47 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -LO $(curl -s https://api.github.com/repos/chrisss404/check-mk-arm/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep buster_armhf.deb)
-dpkg -i check-mk-raw-*.buster_armhf.deb
-rm check-mk-raw-*.buster_armhf.deb
+curl -LO $(curl -s https://api.github.com/repos/chrisss404/check-mk-arm/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep bullseye_armhf.deb)
+dpkg -i check-mk-raw-*.bullseye_armhf.deb
+rm check-mk-raw-*.bullseye_armhf.deb
 /usr/bin/omd stop
 /usr/bin/omd update mon
 /usr/bin/omd start
