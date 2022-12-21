135a29c14b04313a6000942f5900f9b9a6774cf2
Jimmy Bristow <JzJad@users.noreply.github.com>
2022-12-21 08:57:34 -0600

    changes

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ echo "|   _|  _  ||  |______|  __||     |  -__|  __||    <|        ||    < "
 echo "|__| |   __||__|      |____||__|__|_____|____||__|__|__|__|__||__|__|"
 echo "     |__|     "
 if [ ! -d /omd/sites/mon ]; then
-  /bin/bash /app/init.sh
+  # /bin/bash /app/init.sh
+  omd create --no-tmpfs monitoring
 fi
-omd start mon
+omd start monitoring
 sleep infinity
--- upgrade.sh
+++ upgrade.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl -LO $(curl -s https://api.github.com/repos/chrisss404/check-mk-arm/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep jammy_arm64.deb)
+curl -O $(curl -s https://api.github.com/repos/chrisss404/check-mk-arm/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep jammy_arm64.deb)
 dpkg -i check-mk-raw-*.jammy_arm64.deb
 rm check-mk-raw-*.jammy_arm64.deb
 /usr/bin/omd stop
-/usr/bin/omd update mon
+/usr/bin/omd update monitoring
 /usr/bin/omd start
 /usr/bin/omd cleanup
