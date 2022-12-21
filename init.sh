534b90866e63f53a9e7858694b72421470c1d556
Jimmy Bristow <JzJad@users.noreply.github.com>
2022-12-21 00:32:48 -0600

    init change-apk remove

--- init.sh
+++ init.sh
@@ -5,9 +5,9 @@ omd config mon set APACHE_MODE own
 omd config mon set APACHE_TCP_ADDR 0.0.0.0
 omd config mon set APACHE_TCP_PORT 5000
 omd config mon set CORE nagios
+omd update-apache-config mon
 groupadd -g 1000 mon
 useradd -g 1000 -u 1000 -ms /bin/bash -d /omd/sites/mon mon
 usermod -aG mon www-data
 usermod -aG omd mon
-omd enable mon
 omd enable mon
