32b5d1684895688e62f4d006a5924c7ef8337e89
Jimmy Bristow <JzJad@users.noreply.github.com>
2022-12-27 22:57:13 -0600

    Rev

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "First run, creating site monitor..."
-omd create --no-tmpfs monitor
-omd config monitor set APACHE_MODE own
-omd config monitor set APACHE_TCP_ADDR 0.0.0.0
-omd config monitor set APACHE_TCP_PORT 5000
-omd config monitor set CORE nagios
-omd enable monitor
+echo "First run, creating site mon..."
+omd create --no-tmpfs mon
+omd config mon set APACHE_MODE own
+omd config mon set APACHE_TCP_ADDR 0.0.0.0
+omd config mon set APACHE_TCP_PORT 5000
+omd config mon set CORE nagios
+omd enable mon
--- run.sh
+++ run.sh
@@ -4,16 +4,16 @@ echo ".----.-----.|__|______.----.|  |--.-----.----.|  |--.--------.|  |--."
 echo "|   _|  _  ||  |______|  __||     |  -__|  __||    <|        ||    < "
 echo "|__| |   __||__|      |____||__|__|_____|____||__|__|__|__|__||__|__|"
 echo "     |__|     "
-if [ ! -d /omd/sites/monitor ]; then
+if [ ! -d /omd/sites/mon ]; then
   /bin/bash /app/init.sh
 fi
-if ! id monitor &>/dev/null; then
-  groupadd -g 1000 monitor
-  useradd -g 1000 -u 1000 -ms /bin/bash -d /omd/sites/monitor monitor
-  usermod -aG monitor www-data
-  usermod -aG omd monitor
-  omd enable monitor
+if ! id mon &>/dev/null; then
+    groupadd -g 1000 mon
+    useradd -g 1000 -u 1000 -ms /bin/bash -d /omd/sites/mon mon
+    usermod -aG mon www-data
+    usermod -aG omd mon
+    omd enable mon
 else
-omd start monitor
+omd start mon
 sleep infinity
 fi
