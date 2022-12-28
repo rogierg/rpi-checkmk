576c8dfc753ee1ee68ad22c9aa3d68f968e037c9
Jimmy Bristow <JzJad@users.noreply.github.com>
2022-12-27 22:46:40 -0600

    Reverting till new branch is stable

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "First run, creating initial site 'monitoring'..."
-omd create --no-tmpfs monitoring
-
-
+echo "First run, creating site monitor..."
+omd create --no-tmpfs monitor
+omd config monitor set APACHE_MODE own
+omd config monitor set APACHE_TCP_ADDR 0.0.0.0
+omd config monitor set APACHE_TCP_PORT 5000
+omd config monitor set CORE nagios
+omd enable monitor
--- run.sh
+++ run.sh
@@ -4,9 +4,16 @@ echo ".----.-----.|__|______.----.|  |--.-----.----.|  |--.--------.|  |--."
 echo "|   _|  _  ||  |______|  __||     |  -__|  __||    <|        ||    < "
 echo "|__| |   __||__|      |____||__|__|_____|____||__|__|__|__|__||__|__|"
 echo "     |__|     "
-if [ ! -d /omd/sites/mon ]; then
-  # /bin/bash /app/init.sh
-  omd create --no-tmpfs monitoring
+if [ ! -d /omd/sites/monitor ]; then
+  /bin/bash /app/init.sh
 fi
-omd start monitoring
+if ! id monitor &>/dev/null; then
+  groupadd -g 1000 monitor
+  useradd -g 1000 -u 1000 -ms /bin/bash -d /omd/sites/monitor monitor
+  usermod -aG mon www-data
+  usermod -aG omd monitor
+  omd enable monitor
+else
+omd start monitor
 sleep infinity
+fi
